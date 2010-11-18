98dc0e423a42e4245a96ae605a8947f3dce4dee3
mypc1989 <mypc1989@8d23aa85-0a75-fbd3-60da-a1c61123dd02>
2010-11-18 18:01:54 +0000

    Update

---
+++ Project-GiaVang-NgoaiTe-Client/src/UDPCC.java
@@ -0,0 +1,47 @@
+/*
+ * author Nguyen Pham Hoai An
+        */
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+class UDPCC
+{
+	public ArrayList ketNoi(String st) throws Exception
+	{
+            /// tao goi datagramSocket
+		DatagramSocket socket = new DatagramSocket();
+		DatagramPacket packet;
+		byte []data;
+            //
+
+		InetAddress ipS = InetAddress.getByName("localhost");
+		int portS = 2812;
+            // nhap chuoi bat ky tu ban phim;
+		data = st.getBytes();
+             // goi packet den may chu;
+		packet = new DatagramPacket(data,data.length,ipS,portS);
+		socket.send(packet);
+             // nhan packet tu may chu ;
+		data = new byte[1024];
+		packet = new DatagramPacket(data,data.length);
+                System.out.println("assfddf");
+		socket.receive(packet);
+             // in ra console
+		String kq = new String(packet.getData(),0,packet.getLength());
+
+		System.out.println("Chuoi Ket Qua Tu May Chu : " + kq);
+              // dong socket
+		socket.close();
+                int tam = 0;
+                ArrayList a = new ArrayList();
+                for(int i=0;i<kq.length();++i)
+                {
+                    if(kq.charAt(i)== '+') {
+                        tam = i+1;
+                        a.add(kq.substring(tam, i-1));                     
+                    }
+                }
+                return a;
+
+	}
+}
